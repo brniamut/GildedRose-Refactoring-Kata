a4f5e8a7dbeb1ccb87737138202696c821fe91e8
Bastiaan Niamut <bastiaan@kroot.dev>
2024-04-01 20:03:16 +0200

    separate every item name in its own block

--- README.md
+++ README.md
@@ -16,11 +16,11 @@ Just so I can get that good code-style going on
 
 I'm using Vitest because I know about it and want to use its coverage feature. The TextTest tool named in the README.md is also a nice tool, but I'll stick to what I'm used to. The Gilded Rose Requirements suggest writing tests one step at a time and not all at once. I agree with this, but I just want the base tests there, so I know I don't break anything basic. I'm suing the previous named requirements to set up these base tests
 
-### First refactor
+### First Refactor
 
 So let's do some basics first. I'm removing as many nested if statements as possible and try to move around code that uses inverted boolean logic `(a !== b)` to normal comparisons `(a === b)` and exchange the statements in the `if` and `else` block. This creates a better overview and makes it easier to create a mental modal
 
-### Second refactor
+### Second Refactor
 
 The result of the previous step shows me three obvious things:
 
@@ -33,5 +33,12 @@ The quick wins are the last two points so let's do that first
 ### Third Refactor
 
 I notice that every loop this statement is being run `this.items[i].sellIn = this.items[i].sellIn - 1`. This is of course only the case if the item is not 'Sulfuras, Hand of Ragnaros', but we already fixed that. If we move this statement to the top of the loop it breaks the tests for the special items (Tickets and the Brie Cheese). Well that logic was written with the scenario in mind that we would subtract 1 day after the quality checks. So let's change those if statements too by decreasing the check on sellIn by 1.  
+
+### Forth Refactor
+
+Even more clear now is that there are things happening based upon item name and quality. Apart from the previous step the sellIn values is not adjusted anymore.
+The code does things with quality based upon its name. So let's split the loop in a per name part, and let it `continue` if everything is done for that specific item. It will repeat some logic, but will make it clearer to read
+
+
 
 
--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -25,45 +25,44 @@ export class GildedRose {
 
       this.items[i].sellIn = this.items[i].sellIn - 1;
 
-      if (
-        this.items[i].name === 'Aged Brie' ||
-        this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert'
-      ) {
+      if (this.items[i].name === 'Aged Brie') {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
 
-          if (
-            this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert'
-          ) {
-            if (this.items[i].sellIn < 10) {
-              this.items[i].quality = this.items[i].quality + 1;
-            }
-
-            if (this.items[i].sellIn < 5) {
-              this.items[i].quality = this.items[i].quality + 1;
-            }
+          if (this.items[i].sellIn < 0) {
+            this.items[i].quality = this.items[i].quality + 1;
           }
         }
-      } else {
-        if (this.items[i].quality > 0) {
-          this.items[i].quality = this.items[i].quality - 1;
-        }
+
+        continue;
       }
 
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name === 'Aged Brie' && this.items[i].quality < 50) {
+      if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-        } else {
-          if (
-            this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert'
-          ) {
+
+          if (this.items[i].sellIn < 10) {
+            this.items[i].quality = this.items[i].quality + 1;
+          }
+
+          if (this.items[i].sellIn < 5) {
+            this.items[i].quality = this.items[i].quality + 1;
+          }
+
+          if (this.items[i].sellIn < 0) {
             this.items[i].quality =
               this.items[i].quality - this.items[i].quality;
-          } else {
-            if (this.items[i].quality > 0) {
-              this.items[i].quality = this.items[i].quality - 1;
-            }
           }
+        }
+
+        continue;
+      }
+
+      this.items[i].quality = this.items[i].quality - 1;
+
+      if (this.items[i].sellIn < 0) {
+        if (this.items[i].quality > 0) {
+          this.items[i].quality = this.items[i].quality - 1;
         }
       }
     }
